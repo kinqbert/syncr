8392bdeeff7c635ea0eee882105757074a3c3509
kinqbert <perehodko1010@gmail.com>
2026-01-29 01:35:09 +0200

    Refactor code structure for improved readability and maintainability

---
+++ apps/client/src/providers/routerProvider.tsx
@@ -0,0 +1,6 @@
+import { RouterProvider } from "react-router";
+import router from "./routerConfiguration";
+
+const AppRouterProvider = () => <RouterProvider router={router} />;
+
+export default AppRouterProvider;
