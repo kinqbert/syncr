7754b2b186c5502b8ff10402e57f22060b321b94
kinqbert <perehodko1010@gmail.com>
2026-04-29 21:16:30 +0300

    feat: add Inter font files and configure theme with typography settings

---
+++ apps/client/src/lib/theme.ts
@@ -0,0 +1,11 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  typography: {
+    fontFamily: "Inter",
+    button: {
+      textTransform: "none",
+      fontWeight: 500,
+    },
+  },
+});
--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -2,13 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
-
-const theme = createTheme({
-  palette: {
-    mode: "light",
-  },
-});
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { theme } from "./lib/theme";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
