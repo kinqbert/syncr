317583dbbe4c4beeac495cd88406f9fdc08a4dc7
--global <--global>
2026-05-04 17:37:13 +0300

    feat: add simple-import-sort plugin to ESLint configuration

--- apps/client/eslint.config.js
+++ apps/client/eslint.config.js
@@ -2,6 +2,7 @@ import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 import tseslint from "typescript-eslint";
 import { defineConfig, globalIgnores } from "eslint/config";
 import path from "node:path";
@@ -26,6 +27,13 @@ export default defineConfig([
         tsconfigRootDir: __dirname,
         projectService: true,
       },
+    },
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": "warn",
+      "simple-import-sort/exports": "warn",
     },
   },
 ]);
