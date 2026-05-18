8de983f57d722f98b12a7c069262d99b14605a3c
Maksym Perekhodko <perehodko1010@gmail.com>
2026-05-18 18:55:00 +0300

    Merge dev into master (#2)
    * changed tab name to Syncr
    
    * feat: update README
    
    * fix: minor dashboard style fixes
    
    * feat: enhance error handling across various components
    
    * feat: minor design fixes to dashboard and conversations sidebar header

--- apps/client/src/store/useConversationsSidebarStore.ts
+++ apps/client/src/store/useConversationsSidebarStore.ts
@@ -2,20 +2,24 @@ import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
 type ConversationsSidebarStore = {
-  closeSidebar: () => void;
   isOpen: boolean;
+  closeSidebar: () => void;
   openSidebar: () => void;
+  toggleSidebar: () => void;
 };
 
 export const useConversationsSidebarStore = create<ConversationsSidebarStore>()(
   persist(
-    (set) => ({
+    (set, get) => ({
       isOpen: true,
       closeSidebar: () => {
         set({ isOpen: false });
       },
       openSidebar: () => {
         set({ isOpen: true });
+      },
+      toggleSidebar: () => {
+        set({ isOpen: !get().isOpen });
       },
     }),
     { name: "syncr-conversations-sidebar" },
